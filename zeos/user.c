df457d2b475b724329ac5f27b612706d239a1e6f
ignacio <cornet.navarro@gmail.com>
2026-04-11 18:30:37 +0200

    user funcionando

--- zeos/user.c
+++ zeos/user.c
@@ -1,78 +1,76 @@
 #include <libc.h>
 
-static volatile int shared_probe = 111;
-
-static void print_text(char *text)
-{
-  write(1, text, strlen(text));
+void print(char* msg) {
+    write(1, msg, strlen(msg));
 }
 
-static void print_int_line(char *label, int value)
-{
-  char n[16];
-  write(1, label, strlen(label));
-  itoa(value, n);
-  write(1, n, strlen(n));
-  write(1, "\n", 1);
+void print_int(char* msg, int val) {
+    char buff[16];
+    print(msg);
+    itoa(val, buff);
+    print(buff);
+    print("\n");
 }
 
 int __attribute__ ((__section__(".text.main")))
   main(void)
 {
-  int parent_pid = getpid();
-  int ret;
+    print("\n------- ZEOS USER TEST SUITE -------\n");
 
-  print_text("=== TEST FORK ZEOS ===\n");
-  print_int_line("Parent PID: ", parent_pid);
+    int time1 = gettime();
+    print_int("Start time: ", time1);
 
-  print_text("[1] Basic fork return test\n");
-  ret = fork();
-  if (ret < 0) {
-    print_text("fork() failed in basic test\n");
-    perror();
-    for(;;) {}
-  }
+    print("Testing write & getpid...\n");
+    print_int("My PID: ", getpid());
 
-  if (ret == 0) {
-    int child_pid = getpid();
-    print_text("Child branch reached\n");
-    print_int_line("fork() returned: ", ret);
-    print_int_line("Child PID: ", child_pid);
-    shared_probe = 222;
-    for(;;) {}
-  }
+    print("Testing fork...\n");
+    int pid = fork();
 
-  print_text("Parent branch reached\n");
-  print_int_line("fork() returned child PID: ", ret);
-
-  for (volatile int k = 0; k < 20000000; ++k) {}
-  if (shared_probe == 111) print_text("Memory isolation OK\n");
-  else print_text("Memory isolation FAILED\n");
-
-  print_text("[2] Linear multi-fork test (parent-only)\n");
-  {
-    const int max_children = 3;
-    int created = 0;
-
-    for (int i = 0; i < max_children; ++i) {
-      int r = fork();
-      if (r < 0) {
-        print_text("fork() failed in linear test\n");
+    if (pid < 0) {
+        print("Fork failed!\n");
         perror();
-        break;
-      }
-      if (r == 0) {
-        print_text("Linear child started\n");
-        for(;;) {}
-      }
-      created++;
-      print_int_line("Created child PID: ", r);
     }
+    else if (pid == 0) {
+        // Processo Hijo
+        print_int("[Child] Hello! My PID is: ", getpid());
+        // Caso 1: Testeando 'pending_unblocks'
+        print("[Child] Delaying to let parent unblock me first...\n");
+        for (int i = 0; i < 1000000; i++) {}
+        print("[Child] Now calling block(). I should NOT sleep because of pending_unblocks!\n");
+        block();
+        print("[Child] Survived block() successfully!\n");
 
-    print_int_line("Total created in linear test: ", created);
-  }
+        // Caso 2: Bloqueo normal
+        print("[Child] Testing normal block(). Sleeping...\n");
+        block(); 
+        
+        print("[Child] Woke up from normal block! Succesfully unblocked.\n");
+        print_int("[Child] End time: ", gettime());
+        print("[Child] Testing exit(). Goodbye!\n");
+        exit();
+    }
+    else {
+        // Processo Padre
+        print_int("[Parent] I created a child with PID: ", pid);
+        
+        // Caso 1: Testeando 'pending_unblocks'
+        print("[Parent] Testing unblock() on child before it blocks (pending_unblock)...\n");
+        unblock(pid);
 
-  print_text("=== END TEST ===\n");
-
-  for(;;) {}
+        // Caso 2: Bloqueo normal
+        print("[Parent] Delaying to give child time to block normally...\n");
+        for (int i = 0; i < 1000000; i++) {}
+        
+        print("[Parent] Testing unblock() on blocked child...\n");
+        unblock(pid);
+        
+        print("[Parent] Delaying to let child finish...\n");
+        for (int i = 0; i < 1000000; i++) {}
+        
+        print_int("[Parent] End time: ", gettime());
+        print("[Parent] Testing exit(). Goodbye!\n");
+        exit();
+    }
+    
+    while(1);
 }
